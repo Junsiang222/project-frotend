2b4acdbd0d4a07a0fcd0062071527c3c21d40f2f
junsiang <teojunsiang0222@gmail.com>
2025-10-16 10:52:17 +0800

    c

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 // yours
 // export const API_URL = "http://localhost:5524/";
 //nine
-export const API_URL =import.meta.env.DEV?"http://localhost:5524/" : "b15-junsiang.mak3r.dev"
+export const API_URL =import.meta.env.DEV?"http://localhost:5524/api/" : "https://b15-junsiang.mak3r.dev/api/"
